235876cb95360556c0f2dfe9fcf93f62ad435fc5
StephenVarela <varelast92@gmail.com>
2018-10-01 14:31:26 -0400

    changed table name using rename_table works for rollback with later version of rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_181900) do
+ActiveRecord::Schema.define(version: 2018_10_01_182855) do
 
   create_table "new_locations", force: :cascade do |t|
     t.string "name"
@@ -25,7 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_01_181900) do
     t.integer "quantity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
     t.string "dietary_restrictions"
