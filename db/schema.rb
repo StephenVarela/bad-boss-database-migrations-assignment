4d3c7d733e251e1a3b7110dccadfdb52ccaa640d
StephenVarela <varelast92@gmail.com>
2018-10-01 13:47:52 -0400

    added quantity to parts migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_01_173043) do
+ActiveRecord::Schema.define(version: 2018_10_01_173647) do
 
   create_table "parts", force: :cascade do |t|
     t.string "name"
     t.string "manufacturer"
     t.decimal "cost"
+    t.integer "quantity"
   end
 
 end
